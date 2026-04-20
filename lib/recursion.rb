78ae45755621ffc0a71e7d6fdc628374cc716d92
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-20 13:43:34 -0400

    Implement fibs_rec method
    fibs_rec method works and passes all tests

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -15,4 +15,17 @@ def fibs(length)
 end
 
 def fibs_rec(length)
+  arr = Array.new(length)
+
+  if length == 1
+    arr[0] = 0
+  elsif length == 2
+    arr = fibs_rec(1)
+    arr[1] = 1
+  else
+    arr = fibs_rec(length - 1)
+    arr[length - 1] = arr[length - 3] + arr[length - 2]
+  end
+
+  arr
 end
