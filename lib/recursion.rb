7d266103074fe8068d3fac5331a614fd999111ca
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-20 10:26:40 -0400

    Implement fibs method
    fibs method passes all tests

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -1,4 +1,17 @@
 def fibs(length)
+  arr = Array.new(length)
+
+  (0...length).each do |index|
+    arr[index] = if index == 0
+                   0
+                 elsif index == 1
+                   1
+                 else
+                   arr[index - 2] + arr[index - 1]
+                 end
+  end
+
+  arr
 end
 
 def fibs_rec(length)
